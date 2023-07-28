c7a3fb84fa97e8e0a28720ad4eb369106f42aac1
fadygamal <fadygamal5@gmail.com>
2023-07-28 19:05:27 +0300

    commit

--- BankingSystem/src/Bank.java
+++ BankingSystem/src/Bank.java
@@ -1,21 +1,21 @@
 public class Bank implements BankingSystem{
     @Override
     public void creating_accounts() {
-
+        System.out.println("The account created successfully");
     }
 
     @Override
     public void depositing() {
-
+        System.out.println("Depositing done successfully");
     }
 
     @Override
     public void withdrawing_money() {
-
+        System.out.println("withdrawing done successfully");
     }
 
     @Override
     public void viewing_balance() {
-
+        System.out.println("The balance: 200");
     }
 }
