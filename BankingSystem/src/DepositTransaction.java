d950a8227420d3b02657408ebd4946ad0b77b757
fadygamal <fadygamal5@gmail.com>
2023-07-30 17:49:35 +0300

    delet

--- BankingSystem/src/DepositTransaction.java
+++
@@ -1,8 +0,0 @@
-public class DepositTransaction extends Transaction{
-
-    @Override
-    public void perform_transaction() {
-        System.out.print("Deposit ");
-        super.perform_transaction();
-    }
-}
