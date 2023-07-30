13c609c16f3cff8a3ffb06862f58a89bfe2f7662
fadygamal <fadygamal5@gmail.com>
2023-07-30 19:16:49 +0300

    del

--- BankingSystem/src/Transaction.java
+++
@@ -1,7 +0,0 @@
-public abstract class Transaction {
-
-     public void perform_transaction(){
-         System.out.println("transaction done successfully");
-     }
-
-}
--- BankingSystem/src/WithdrawTransaction.java
+++
@@ -1,7 +0,0 @@
-public class WithdrawTransaction extends Transaction{
-    @Override
-    public void perform_transaction() {
-        System.out.print("Withdraw ");
-        super.perform_transaction();
-    }
-}
